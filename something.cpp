6f72c0ee01573ce5b15a66665c4d0b6ca11e563a
Eric Schles <ericschles@gmail.com>
2013-03-18 16:00:49 -0400

    add something.cpp

---
+++ something.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+	vector<int> a;
+
+	for(int i = 0; i < 20; i++)
+		a.push_back(i);
+
+	for(int j = 0; j< 20;j++)
+		cout << a[i]
+
+
+
+
+	return 0;
+}
